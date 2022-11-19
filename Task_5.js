2d5d6d8397ac968123343e946d91406a4823210a
Sardor0795 <english.programmer.9595@gmail.com>
2022-11-19 13:45:45 +0500

    While, Do While loops

--- Task_5.js
+++ Task_5.js
@@ -6,3 +6,28 @@
 //   console.log(i);
 //   i--;
 // }
+
+// Ex 2
+
+// let i = 0;
+
+// while (++i < 5) {
+//   console.log(i);
+// }
+
+// Ex 3
+
+// let i = 0;
+
+// while (i++ < 5) {
+//   console.log(i);
+// }
+
+// Ex 4
+
+// let i = 0;
+
+// while (i < 3) {
+//   console.log(`Number: ${i}!`);
+//   i++;
+// }
