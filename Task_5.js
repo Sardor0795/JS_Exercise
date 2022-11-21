a44dffc05d27b12556b939b0a5324cbc34bac2e9
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2022-11-21 18:51:27 +0500

    While, Do While loops, Function

--- Task_5.js
+++ Task_5.js
@@ -1,29 +1,4 @@
-// Ex 1
-
-// let i = 3;
-
-// while (i) {
-//   console.log(i);
-//   i--;
-// }
-
-// Ex 2
-
-// let i = 0;
-
-// while (++i < 5) {
-//   console.log(i);
-// }
-
-// Ex 3
-
-// let i = 0;
-
-// while (i++ < 5) {
-//   console.log(i);
-// }
-
-// Ex 4
+// Ex 1 ---
 
 // let i = 0;
 
@@ -31,3 +6,15 @@
 //   console.log(`Number: ${i}!`);
 //   i++;
 // }
+
+// Ex 2 ---
+
+// const checkAge = (age) => {
+//   return age > 18 ? true : "Did parents allow you ?";
+// };
+
+// const checkAge = (age) => {
+//   return age > 18 || "Did parents allow you ?";
+// };
+
+// console.log(checkAge(19));
