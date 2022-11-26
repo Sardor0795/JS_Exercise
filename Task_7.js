e2c96fbce8f011d623f080722eaa84df4b5f7b4c
Sardor0795 <english.programmer.9595@gmail.com>
2022-11-26 13:56:12 +0500

    Objects Advanced | Function Constructor

--- Task_7.js
+++ Task_7.js
@@ -52,3 +52,27 @@
 // };
 
 // ladder.up().up().down().showStep().down().showStep();
+
+// Ex 4
+
+// Ex 4 ---
+
+// let obj = {
+//   name: "Sardor",
+//   age: 27,
+// };
+
+// function A() {
+//   return obj;
+// }
+// function B() {
+//   return obj;
+// }
+
+// let a = new A();
+// let b = new B();
+
+// console.log(a);
+// console.log(b);
+
+// console.log(a == b);
