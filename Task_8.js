59e538c4c98c69743adf5c28fd93226b0a9054d2
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2022-11-29 07:03:14 +0500

    Number, String

---
+++ Task_8.js
@@ -0,0 +1,16 @@
+// Ex 1
+
+// let num = 20;
+
+// let binary = num.toString(2);
+
+// let sum_1 = null;
+// let sum_2 = null;
+
+// for (let i = 0; i < binary.length; i++) {
+//   +binary[i] == 1 ? sum_1 += 1 : sum_2 += 1;
+// }
+
+// console.log(binary);
+// console.log(sum_1);
+// console.log(sum_2);
