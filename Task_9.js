1188f8f828e63d7a08f2e42da142d9405fd5833b
Sardor0795 <english.programmer.9595@gmail.com>
2022-12-01 10:55:24 +0500

    Array

---
+++ Task_9.js
@@ -0,0 +1,15 @@
+// Ex 1 ---
+
+// let arr = [];
+
+// for (let i = 1; i <= 10; i++) {
+//   arr.push(parseInt(Math.random() * 100));
+// }
+
+// let sumArr = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//   sumArr += arr[i];
+// }
+
+// console.log(sumArr);
