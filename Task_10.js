3566268b01089b5e9e34ff2d76e733238f7eaeae
Sardor0795 <english.programmer.9595@gmail.com>
2022-12-03 10:07:41 +0500

    Array advanced

---
+++ Task_10.js
@@ -0,0 +1,15 @@
+let users = [
+  { id: 1, year: 1998, engine: 1, name: "Tico" },
+  { id: 2, year: 2005, engine: 1.2, name: "Matiz" },
+  { id: 3, year: 2010, engine: 1.6, name: "Gentra" },
+  { id: 4, year: 2010, engine: 1.5, name: "Cobalt" },
+  { id: 5, year: 2012, engine: 2, name: "Malibu" },
+  { id: 6, year: 2000, engine: 1.2, name: "Damas" },
+  { id: 7, year: 2018, engine: 2.4, name: "Tracker" },
+];
+
+let before2010 = users.filter((v) => v.year < 2000);
+let after2010 = users.filter((v) => v.year > 2010);
+
+console.log(before2010);
+console.log(after2010);
