7f7dad4ef9f282afd18b1f6ac5a161daa71922fe
Sardor0795 <english.programmer.9595@gmail.com>
2022-12-06 12:48:15 +0500

    CRUD

--- Task_11.js
+++ Task_11.js
@@ -27,3 +27,25 @@
 //     console.log(i);
 //   }
 // }
+
+// No3
+// object valuelarining boolean tipidagi malumotlrini qaytaring keylari bn biriga
+// chiqish
+// {   offline:true,
+//     online:true,
+//     individual:false
+// }
+
+// let obj = {
+//   id: 1,
+//   name: "WebBrain",
+//   offline: true,
+//   online: true,
+//   individual: false,
+// };
+
+// for (i in obj) {
+//   if (typeof obj[i] == "boolean") {
+//     console.log(i);
+//   }
+// }
