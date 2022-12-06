2242d0e0b8d9dc79d0aa36fbb3e8548575af3866
Sardor0795 <english.programmer.9595@gmail.com>
2022-12-06 20:16:30 +0500

    CRUD

--- Task_11.js
+++ Task_11.js
@@ -1,4 +1,4 @@
-// Ex 1
+// Ex 1 ---
 
 // let str = "webbbraiiin acccademmmy";
 
@@ -11,3 +11,31 @@
 // }
 
 // console.log(resStr);
+
+// Ex 2 ---
+
+// let str = "ikki";
+
+// let res = str.split("").reverse("").join("") === str;
+
+// console.log(res);
+
+// Ex 3 ---
+
+// let str = "Webbrain Academy";
+
+// const calcChars = (str) => {
+//   let minChars = 0;
+//   let bigChars = 0;
+
+//   for (let i of str) {
+//     if (i !== " ") {
+//       if (i === i.toLowerCase()) ++minChars;
+//       else ++bigChars;
+//     }
+//   }
+
+//   return `Big chars: ${bigChars}, Min chars: ${minChars}`;
+// };
+
+// console.log(calcChars(str));
