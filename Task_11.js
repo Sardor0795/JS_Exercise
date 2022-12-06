3f6ebbf087de5a64bb9eec54d39c14cc8aa7a252
Sardor0795 <english.programmer.9595@gmail.com>
2022-12-06 12:45:28 +0500

    CRUD

--- Task_11.js
+++ Task_11.js
@@ -11,3 +11,19 @@
 // for (let i in obj) {
 //   console.log(i);
 // }
+
+// no2
+// object valuelarining raqamga tenglarini toping
+// chiqish
+// id
+
+// let obj = {
+//   id: 1,
+//   name: "WebBrain",
+// };
+
+// for (let i in obj) {
+//   if (typeof obj[i] == "number") {
+//     console.log(i);
+//   }
+// }
