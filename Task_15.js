3510dc90eb8e5b6211c73f2d47c0bf5b6b3ee265
Sardor0795 <english.programmer.9595@gmail.com>
2022-12-18 21:02:56 +0500

    Prototype

---
+++ Task_15.js
@@ -0,0 +1,16 @@
+// Ex: 1
+
+// let obj = {
+//   name: "Sardor",
+//   age: 27,
+// };
+
+// Object.prototype.dell = function (obj, key) {
+//   delete obj[key];
+// };
+
+// console.log(obj);
+
+// Object.dell(obj, "age");
+
+// console.log(obj);
