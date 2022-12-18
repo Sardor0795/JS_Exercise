f816ed083f070336d6e1cf190660d58b66f805d3
Sardor0795 <english.programmer.9595@gmail.com>
2022-12-18 21:24:29 +0500

    Prototype

--- Task_15.js
+++ Task_15.js
@@ -14,3 +14,18 @@
 // Object.dell(obj, "age");
 
 // console.log(obj);
+
+// Ex: 2
+
+// let obj = {
+//   name: "Sardor",
+//   age: 27,
+// };
+
+// Object.prototype.setProto = function (obj, key, type) {
+//   Object.defineProperty(obj, key, type);
+// };
+
+// Object.setProto(obj, "age", { configurable: false });
+
+// console.log(Object.getOwnPropertyDescriptor(obj, "age"));
