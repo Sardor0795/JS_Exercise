c51685ad3ceee554198e5b9003b59fbefb0f85c6
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-01-08 12:25:29 +0500

    DOM

---
+++ task-18/2/script.js
@@ -0,0 +1,7 @@
+let img = document.querySelector(".cl_btn_img");
+let ul = document.querySelector("ul");
+
+const clFunc = () => {
+  ul.classList.toggle("none");
+  img.classList.toggle("rotate");
+};
